ddbaf0af98d60c293f7f99d60f18493b5829cff0
pbg2h15are <pbg2h15are@pb.bib.de>
2016-12-16 12:32:28 +0100

    Upload Control.java Upload Debuff.java Upload DebuffType.java (Enum) Upload PowerUp.java Upload PowerUpType.java (Enum)

---
+++ EoA/src/de/bib/pbg2h15a/DebuffType.java
@@ -0,0 +1,34 @@
+package de.bib.pbg2h15a;
+
+public enum DebuffType {
+	
+	SPEED_DOWN("Speed Down", -1),
+	BOMB_MINUS("Bomb Minus", -1),
+	RANGE_MINUS("Range Minus", -1);
+	
+	private String description;
+	private int value;
+	
+	private DebuffType(String description, int value) {
+		this.description = description;
+		this.value = value;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
+}
