daa7a0c4d08ce1ab068730a89a72c2223d9eeab6
pbd2h15aho <pbd2h15aho@pb.bib.de>
2016-12-19 10:16:11 +0100

    Aktuallisierung der GUI und hinzufügen von Grafiken

--- EoA/src/de/bib/pbg2h15a/GUI.java
+++ EoA/src/de/bib/pbg2h15a/GUI.java
@@ -1,238 +1,396 @@
 package de.bib.pbg2h15a;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class GUI{
 	/**
 	 * @author pbd2h15aho
-	 */
+	 */	
 	
-	private Texture txTimer;
-	private Texture txP1;
-	private Texture txP2;
-	private Texture txP3;
-	private Texture txP4;
+	private static final float SCALE_X=Gdx.graphics.getWidth()/100;
+	private static final float SCALE_Y=Gdx.graphics.getHeight()/100;
+	
+	private static final String[] PFADE={"img/Buttons/GUI/Timer_BGLabel.png"				//0
+									   	,"img/Buttons/GUI/SpielerDead_BGLabel.png"
+									   	,"img/Buttons/GUI/SpielerDead_BGLabel2.png"
+									   	,"img/Buttons/GUI/Spieler1_BGLabel.png"
+									   	,"img/Buttons/GUI/Spieler2_BGLabel.png"
+									   	,"img/Buttons/GUI/Spieler3_BGLabel.png"				//5
+									   	,"img/Buttons/GUI/Spieler4_BGLabel.png"
+									   	,"img/Buttons/GUI/ZurueckIconButton.png"
+									   	,"img/Buttons/GUI/PauseIconButton.png"
+									   	,"img/Buttons/GUI/ZurueckIconButtonPressed.png"
+									   	,"img/Buttons/GUI/PauseIconButtonPressed.png"		//10
+									   	,"img/Buttons/GUI/BombeButton.png"
+									   	,"img/Buttons/GUI/UpButton.png"
+									   	,"img/Buttons/GUI/DownButton.png"
+									   	,"img/Buttons/GUI/RightButton.png"
+									   	,"img/Buttons/GUI/LeftButton.png"					//15
+									   	,"img/Buttons/GUI/BombeButtonPressed.png"
+									   	,"img/Buttons/GUI/UpButtonPressed.png"
+									   	,"img/Buttons/GUI/DownButtonPressed.png"
+									   	,"img/Buttons/GUI/RightButtonPressed.png"
+									   	,"img/Buttons/GUI/LeftButtonPressed.png"			//20
+										};
+	
+	//Label
+	private Label lbTimer;
+	private Label lbP1;
+	private Label lbP2;
+	private Label lbP3;
+	private Label lbP4;
+	
+	//Images
+	private Image imTimer;
+	private Image[] imgsPlayers;
+	
+	//Textures
 	private Texture txDP;
+	private Texture txDP2;
 	
-	//MenüButtons
-	private Texture txBtnBack;
-	private Texture txBtnPause;
-	
-	private TextureRegion trBtnBack;
-	private TextureRegion trBtnPause;
-	
+	//TextureRegionDrawables
+	private TextureRegionDrawable tdDP;
+	private TextureRegionDrawable tdDP2;
 	private TextureRegionDrawable tdBtnBack;
 	private TextureRegionDrawable tdBtnPause;
-	
-	private ImageButton iBtnBack;
-	private ImageButton iBtnPause;
-	
-	//SteuerungsButtons
-	private Texture txBtnBomb;
-	private Texture txBtnUp;
-	private Texture txBtnDown;
-	private Texture txBtnRight;
-	private Texture txBtnLeft;
-	
-	private TextureRegion trBtnBomb;
-	private TextureRegion trBtnUp;
-	private TextureRegion trBtnDown;
-	private TextureRegion trBtnRight;
-	private TextureRegion trBtnLeft;
-	
-	
 	private TextureRegionDrawable tdBtnBomb;
 	private TextureRegionDrawable tdBtnUp;
 	private TextureRegionDrawable tdBtnDown;
 	private TextureRegionDrawable tdBtnRight;
 	private TextureRegionDrawable tdBtnLeft;
 	
+	private TextureRegionDrawable tdBtnBackPressed;
+	private TextureRegionDrawable tdBtnPausePressed;
+	private TextureRegionDrawable tdBtnBombPressed;
+	private TextureRegionDrawable tdBtnUpPressed;
+	private TextureRegionDrawable tdBtnDownPressed;
+	private TextureRegionDrawable tdBtnRightPressed;
+	private TextureRegionDrawable tdBtnLeftPressed;
+	
+	
+	//ImageButtons
+	private ImageButton iBtnBack;
+	private ImageButton iBtnPause;
 	private ImageButton iBtnBomb;
 	private ImageButton iBtnUp;
 	private ImageButton iBtnDown;
 	private ImageButton iBtnRight;
 	private ImageButton iBtnLeft;
 	
-	//Tabels
+	//Tables
 	private Table tblMenueBtns;
+	private Table tblLabels;
+	private Table tblSteuerungBnt;
 	
-	//Schrift
+	//Font
 	private BitmapFont bitfont;
 	
-	//Objekte
+	//Objects
+	private Stage stage;
 	private Timer time;
-	private Player p1; 
-	private Player p2; 
-	private Player p3;
-	private Player p4;
-	private static final int P_ANZEIGE_ABSTAND_TOP=80;
 	
+	private Player[] players;
+	private boolean[] alife={true,true,true,true};
 	
+	/**
+	 * @author pbd2h15aho
+	 * The GUI is the Graphical User Interface, which enables the User to control the game through mouse controls
+	 */	
 	public GUI(Timer time, Player p1, Player p2, Player p3, Player p4) {
 		super();
 		this.time = time;
-		this.p1 = p1;
-		this.p2 = p2;
-		this.p3 = p3;
-		this.p4 = p4;
+		players= new Player[4];
+		players[0]=p1;
+		players[1]=p2;
+		players[2]=p3;
+		players[3]=p4;
 		
-		txTimer= new Texture("img/Buttons/GUI/Timer_BGLabel.png");
-		txP1= new Texture("img/Buttons/GUI/Spieler1_BGLabel.png");
-		txP2= new Texture("img/Buttons/GUI/Spieler2_BGLabel.png");
-		txP3= new Texture("img/Buttons/GUI/Spieler3_BGLabel.png");
-		txP4= new Texture("img/Buttons/GUI/Spieler4_BGLabel.png");
-		txDP= new Texture("img/Buttons/GUI/SpielerDead_BGLabel.png");
+		txDP= new Texture(PFADE[1]);
+		txDP2= new Texture(PFADE[2]);
+		tdDP=new TextureRegionDrawable(new TextureRegion(txDP));
+		tdDP2=new TextureRegionDrawable(new TextureRegion(txDP2));
 		
-		//Menü Buttons
-		txBtnBack =new Texture("img/Buttons/GUI/ZurueckIconButton.png");
-		txBtnPause =new Texture("img/Buttons/GUI/PauseIconButton.png");
-		
-		trBtnBack= new TextureRegion(txBtnBack);
-		trBtnPause= new TextureRegion(txBtnPause);
-		
-		tdBtnBack= new TextureRegionDrawable(trBtnBack);
-		tdBtnPause= new TextureRegionDrawable(trBtnPause);
-		
-		iBtnBack= new ImageButton(tdBtnBack);
-		iBtnPause=new ImageButton(tdBtnPause);
-		
-		
-		//Steuerungs Buttons
-		txBtnBomb =new Texture("img/Buttons/GUI/BombeButton.png");
-		txBtnUp =new Texture("img/Buttons/GUI/UpButton.png");
-		txBtnDown =new Texture("img/Buttons/GUI/DownButton.png");
-		txBtnRight =new Texture("img/Buttons/GUI/RightButton.png");
-		txBtnLeft =new Texture("img/Buttons/GUI/LeftButton.png");
-		
-		trBtnBomb= new TextureRegion(txBtnBomb);
-		trBtnUp= new TextureRegion(txBtnUp);
-		trBtnDown= new TextureRegion(txBtnDown);
-		trBtnRight= new TextureRegion(txBtnRight);
-		trBtnLeft= new TextureRegion(txBtnLeft);
-		
-		tdBtnBomb= new TextureRegionDrawable(trBtnBomb);
-		tdBtnUp= new TextureRegionDrawable(trBtnUp);
-		tdBtnDown= new TextureRegionDrawable(trBtnDown);
-		tdBtnRight= new TextureRegionDrawable(trBtnRight);
-		tdBtnLeft= new TextureRegionDrawable(trBtnLeft);
-		
-		iBtnBomb= new ImageButton(tdBtnBomb);
-		iBtnUp= new ImageButton(tdBtnUp);
-		iBtnDown= new ImageButton(tdBtnDown);
-		iBtnRight= new ImageButton(tdBtnRight);
-		iBtnLeft= new ImageButton(tdBtnLeft);
-		
-		//Schrift
 		bitfont = new BitmapFont();
 		bitfont.setColor(Color.WHITE);
+
+		createMenuButtons();
+		createControlButtons();
+		createLabels();
+		createImages();
+		createListeners();
 		
-		//Tabels
-		tblMenueBtns= new Table();
-		tblMenueBtns.add(iBtnBack);
-		tblMenueBtns.add(iBtnPause);
-		tblMenueBtns.setFillParent(true);
-		tblMenueBtns.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		tblMenueBtns.setPosition(Gdx.graphics.getWidth()/2-80,Gdx.graphics.getHeight()/2-40);
-		
-		
-		
-		//Button Funktionalität
-		//TODO zurück zum Menü
-		iBtnBack.addListener(new EventListener() {	
-			@Override
-			public boolean handle(Event event) {
-				if(iBtnBack.isPressed()){
-					p1.setLife(1);
-				}
-				return false;
-			}
-		});
-		
-		//TODO PauseMenü
-		iBtnPause.addListener(new EventListener() {	
-			@Override
-			public boolean handle(Event event) {
-				if(iBtnPause.isPressed()){
-					p1.setLife(0);
-				}
-				return false;
-			}
-		});
-		
-		//TODO Verknüpfen mit move up
-		
-		
-		
-		
-		
-		//TODO Verknüpfen mit move down
-		
-		
-		
-		//TODO Verknüpfen mit move left
-		
-		
-		
-		//TODO Verknüpfen mit move right
-		
-		
-		
+		stage= new Stage();
+		Gdx.input.setInputProcessor(stage);
+		for(int i=0; i<imgsPlayers.length;i++){
+			stage.addActor(imgsPlayers[i]);
+		}
+		stage.addActor(imTimer);
+		stage.addActor(lbTimer);
+		stage.addActor(tblLabels);
+		stage.addActor(tblMenueBtns);
+		stage.addActor(tblSteuerungBnt);
+		stage.addActor(iBtnBomb);
 		
 	}
 	
+	/**
+	 * @author pbd2h15aho
+	 * The public method render renders the GUI 
+	 */	
 	public void render(SpriteBatch sb){
+		bitfont.draw(sb,"Puffer", 0, 0);
 		
-		//PositionTimer
-		sb.draw(txTimer,  Gdx.graphics.getWidth()/2-txTimer.getWidth()/2-3, Gdx.graphics.getHeight()-txTimer.getHeight()-7);
-		String text=""+(int)time.getTime();
-		bitfont.draw(sb, text,(Gdx.graphics.getWidth()/2)-text.length()*3, Gdx.graphics.getHeight()-30);
-		
-		//Position Spielerdaten
-		//Player1
-		isAlifeAnzeigen(p1, sb, txP1,150);
-		bitfont.draw(sb, p1.getName(),115, Gdx.graphics.getHeight()-95);
-		
-		//Player2
-		isAlifeAnzeigen(p2, sb, txP2,350);
-		bitfont.draw(sb, p2.getName(),315, Gdx.graphics.getHeight()-95);
-		
-		//Player3
-		isAlifeAnzeigen(p3, sb, txP3,Gdx.graphics.getWidth()-350);
-		bitfont.draw(sb, p3.getName(),Gdx.graphics.getWidth()-415, Gdx.graphics.getHeight()-95);
-		
-		//Player4
-		isAlifeAnzeigen(p4, sb, txP4,Gdx.graphics.getWidth()-150);
-		bitfont.draw(sb, p4.getName(),Gdx.graphics.getWidth()-215, Gdx.graphics.getHeight()-95);
-		
-		
-		//Position Buttons
-		tblMenueBtns.draw(sb, 1);
-		
-		
+		lbTimer.setText(""+(int)time.getTime());
+		showIsAlife();
+		stage.draw();	
 	}
 	
-	
-	private void isAlifeAnzeigen(Player p,SpriteBatch sb,Texture t,int x){
-		//TODO evt mit einer Methode is alive verknüpfen
-		if(p.getLife()>0){
-			sb.draw(t, x-t.getWidth()/2, Gdx.graphics.getHeight()-t.getHeight()-P_ANZEIGE_ABSTAND_TOP);
-		}else{
-			sb.draw(txDP, x-txDP.getWidth()/2, Gdx.graphics.getHeight()-txDP.getHeight()-P_ANZEIGE_ABSTAND_TOP);
+	/**
+	 * @author pbd2h15aho
+	 *isAlifeAnzeigen() is a private method, which changes the BackgroundImages connected to the Players number of lives.
+	 */	
+	private void showIsAlife(){
+		//TODO connect to Player.isAlife
+		for(int i=0; i<imgsPlayers.length;i++){
+			if(players[i].getLife()==0&&alife[i]){
+				if(i<2){
+					imgsPlayers[i].setDrawable(tdDP);
+				}else{
+					imgsPlayers[i].setDrawable(tdDP2);
+				}
+				alife[i]=false;
+			}
 		}
 	}
-	
 
+	/**
+	 * @author pbd2h15aho
+	 * The private method createMenueButtons initializes the Buttons which are connected to the Menu
+	 * and sets their position on the stage
+	 */	
+	private void createMenuButtons(){
+		tdBtnBack= 	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[7])));
+		tdBtnPause= new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[8])));
+		tdBtnBackPressed= 	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[9])));
+		tdBtnPausePressed= 	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[10])));
+		
+		iBtnBack= new ImageButton(tdBtnBack,tdBtnBackPressed);
+		iBtnPause=new ImageButton(tdBtnPause,tdBtnPausePressed);
+		
+		tblMenueBtns= new Table();
+		tblMenueBtns.add(iBtnBack);
+		tblMenueBtns.add().size(2*SCALE_X,0);
+		
+		//TODO Check if NetworkGame or SinglePlayer
+		tblMenueBtns.add(iBtnPause);
+		
+		tblMenueBtns.setFillParent(true);
+		tblMenueBtns.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		tblMenueBtns.setPosition(Gdx.graphics.getWidth()/2-SCALE_X*8 , Gdx.graphics.getHeight()/2-5.3f*SCALE_Y);
+	}
+	
+	/**
+	 * @author pbd2h15aho
+	 * The private method createLabels initializes the Labels which are connected to the Players and the Timer
+	 * and sets their position on the stage
+	 */	
+	private void createLabels(){
+		Label.LabelStyle lbStyle=new Label.LabelStyle();
+		lbStyle.font=bitfont;
+		lbStyle.fontColor=Color.WHITE;
+		String text=""+(int)time.getTime();
+		
+		lbTimer=new Label(text,lbStyle);
+		lbTimer.setPosition(Gdx.graphics.getWidth()/2-text.length()*3, Gdx.graphics.getHeight()-6*SCALE_Y);
+		
+		lbP1= new Label(players[0].getName(),lbStyle);
+		lbP2= new Label(players[1].getName(),lbStyle);
+		lbP3= new Label(players[2].getName(),lbStyle);
+		lbP4= new Label(players[3].getName(),lbStyle);
+		
+		tblLabels= new Table();
+		tblLabels.add(lbP1).size(15*SCALE_X);
+		tblLabels.add().size(5*SCALE_X);
+		tblLabels.add(lbP2).size(15*SCALE_X);
+		tblLabels.add().size(12.5f*SCALE_X);
+		tblLabels.add(lbP3).size(15*SCALE_X);
+		tblLabels.add().size(5*SCALE_X);
+		tblLabels.add(lbP4).size(10*SCALE_X);
+		tblLabels.setFillParent(true);
+		tblLabels.setPosition(0,Gdx.graphics.getHeight()/2-13.6f*SCALE_Y);
+	}
+	
+	/**
+	 * @author pbd2h15aho
+	 * The private method createControlButtons initializes the Buttons which are connected to the Controls
+	 * and sets their position on the stage
+	 */	
+	private void createControlButtons(){
+		tdBtnBomb= 	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[11])));
+		tdBtnUp= 	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[12])));
+		tdBtnDown= 	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[13])));
+		tdBtnRight= new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[14])));
+		tdBtnLeft= 	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[15])));
+		tdBtnBombPressed=	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[16])));
+		tdBtnUpPressed=		new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[17])));
+		tdBtnDownPressed=	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[18])));
+		tdBtnRightPressed=	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[19])));
+		tdBtnLeftPressed=	new TextureRegionDrawable(new TextureRegion(new Texture(PFADE[20])));
+				
+		iBtnBomb= 	new ImageButton(tdBtnBomb,tdBtnBombPressed);
+		iBtnUp= 	new ImageButton(tdBtnUp,tdBtnUpPressed);
+		iBtnDown= 	new ImageButton(tdBtnDown,tdBtnDownPressed);
+		iBtnRight= 	new ImageButton(tdBtnRight,tdBtnRightPressed);
+		iBtnLeft= 	new ImageButton(tdBtnLeft,tdBtnLeftPressed);
+				
+		iBtnBomb.setPosition(Gdx.graphics.getWidth()-12.5f*SCALE_X,10*SCALE_Y);
+				
+		tblSteuerungBnt= new Table();
+		tblSteuerungBnt.add();
+		tblSteuerungBnt.add(iBtnUp);
+		tblSteuerungBnt.add();
+		tblSteuerungBnt.row();
+		tblSteuerungBnt.add(iBtnLeft);
+		tblSteuerungBnt.add();
+		tblSteuerungBnt.add(iBtnRight);
+		tblSteuerungBnt.row();
+		tblSteuerungBnt.add();
+		tblSteuerungBnt.add(iBtnDown);
+		tblSteuerungBnt.add();
+			
+		tblSteuerungBnt.setPosition(8.8f*SCALE_X,20*SCALE_Y);
+	}
+	
+	/**
+	 * @author pbd2h15aho
+	 * The private method createImages initializes the Images
+	 * and sets their position on the stage
+	 */	
+	private void createImages() {
+		
+		imTimer= new Image(new Texture(PFADE[0]));
+		imTimer.setPosition(Gdx.graphics.getWidth()/2-imTimer.getWidth()/2-0.3f*SCALE_X,
+							Gdx.graphics.getHeight()-imTimer.getHeight()-0.93f*SCALE_Y);
+		
+		imgsPlayers= new Image[4];
+		imgsPlayers[0] = new Image(new Texture(PFADE[3]));
+		imgsPlayers[1] = new Image(new Texture(PFADE[4]));
+		imgsPlayers[2] = new Image(new Texture(PFADE[5]));
+		imgsPlayers[3] = new Image(new Texture(PFADE[6]));
+		
+		imgsPlayers[0].setPosition(15*SCALE_X-imgsPlayers[0].getWidth()/2, 
+								   Gdx.graphics.getHeight()-imgsPlayers[0].getHeight()-10.6f*SCALE_Y);
+		imgsPlayers[1].setPosition(35*SCALE_X-imgsPlayers[1].getWidth()/2, 
+								   Gdx.graphics.getHeight()-imgsPlayers[1].getHeight()-10.6f*SCALE_Y);
+		imgsPlayers[2].setPosition(Gdx.graphics.getWidth()-35*SCALE_X-imgsPlayers[2].getWidth()/2,
+								   Gdx.graphics.getHeight()-imgsPlayers[2].getHeight()-10.6f*SCALE_Y);
+		imgsPlayers[3].setPosition(Gdx.graphics.getWidth()-15*SCALE_X-imgsPlayers[3].getWidth()/2, 
+								   Gdx.graphics.getHeight()-imgsPlayers[3].getHeight()-10.6f*SCALE_Y);
+	}
+	
+	/**
+	 * @author pbd2h15aho
+	 * The private method createListeners initializes the Buttons Listeners and connects them to their functionalities
+	 */	
+	private void createListeners(){ 
+		//TODO back to Menu
+			iBtnBack.addListener(new InputListener() {
+				@Override
+				public boolean handle(Event event) {
+					if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+						System.out.println("Back:click");
+						return true;
+					}
+					
+					return false;
+				}
+			});
+			
+		//TODO connect with PauseMenu
+			iBtnPause.addListener(new InputListener() {	
+				@Override
+				public boolean handle(Event event) {
+					if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+						System.out.println("Pause:click");
+						return true;
+					}
+					return false;}
+			});
+
+		//TODO connect to  moveup()
+			iBtnUp.addListener(new InputListener() {	
+				@Override
+				public boolean handle(Event event) {
+					if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+						
+						System.out.println("Up:click");
+						return true;
+					}
+					return false;
+				}
+			});
+			
+		//TODO connect to movedown()
+			iBtnDown.addListener(new InputListener() {	
+				@Override
+				public boolean handle(Event event) {
+					if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+						System.out.println("Down:click");
+						return true;
+					}
+					return false;
+				}
+			});
+			
+		//TODO connect to move left
+			iBtnLeft.addListener(new InputListener() {	
+				@Override
+				public boolean handle(Event event) {
+					if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+						System.out.println("Left:click");
+						return true;
+					}
+					return false;
+				}
+			});
+			
+		//TODO connect to move right
+			iBtnRight.addListener(new InputListener() {	
+				@Override
+				public boolean handle(Event event) {
+					if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+						System.out.println("Right:click");
+						return true;
+					}
+					return false;
+				}
+			});
+		
+		//TODO connect to dropBomb
+			iBtnBomb.addListener(new InputListener() {	
+				@Override	
+				public boolean handle(Event event) {
+					if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+						System.out.println("Bomb:click");
+						return true;
+					}
+					return false;
+				}
+			});
+	}
+	
 }
