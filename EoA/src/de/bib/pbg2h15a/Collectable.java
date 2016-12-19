bee6d86e7a401f59409204a850f59f216df6d086
pbg2h15afa <pbg2h15afa@pb.bib.de>
2016-12-19 16:45:01 +0100

    Erstellung der abstrakten Klasse Collectable Erstellung der Klasse Wall

---
+++ EoA/src/de/bib/pbg2h15a/Collectable.java
@@ -0,0 +1,15 @@
+package de.bib.pbg2h15a;
+
+import java.awt.Point;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class Collectable extends GameObject {
+
+	public Collectable(int x, int y, boolean passable, Texture spritesheet) {
+		super(new Point(x, y), true, null);
+		// TODO Auto-generated constructor stub
+	}
+
+}
