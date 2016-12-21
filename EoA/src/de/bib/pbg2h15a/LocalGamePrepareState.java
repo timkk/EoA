b4f3d18e4aa19b04422558b7063ff147c680d091
pbg2h15ala <pbg2h15ala@pb.bib.de>
2016-12-21 10:14:52 +0100

    LocalGamePrepareState: Alle Funktionen implementiert. Es fehlen noch die Grafiken und die Möglichkeit die eingestellten Daten auf das Spiel zu übertragen.

--- EoA/src/de/bib/pbg2h15a/LocalGamePrepareState.java
+++ EoA/src/de/bib/pbg2h15a/LocalGamePrepareState.java
@@ -3,7 +3,13 @@ package de.bib.pbg2h15a;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+
+
 
 /**
  * 
@@ -12,27 +18,41 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LocalGamePrepareState extends GameState {
 	
+	private Stage stage;
+	Skin skinName;
+	
+	
+	int mapNr=1;
+	int rundenZeitMin=3;
+	int rundenZeitSek=30;
+	int rundenAnzahl =3;
+
 	// Textures
-		ButtonErstellen name;
-		ButtonErstellen changeStage;
+	ButtonErstellen name;
+	private TextField txfName;	
+
+	ButtonErstellen changeStage;
+	ButtonErstellen changeStageLeft;
+	ButtonErstellen changeStageRight;
+	texHelper changeStageValue;
+
+	ButtonErstellen runden;
+	ButtonErstellen rundenLeft;
+	ButtonErstellen rundenRight;
+	texHelper rundenValue1;
+	texHelper rundenValue10;
+
+	ButtonErstellen zeit;
+	ButtonErstellen zeitLeft;
+	ButtonErstellen zeitRight;
+	BitmapFont zeitValue;
+
 		
-		ButtonErstellen runden;
-		ButtonErstellen rundenLeft;
-		ButtonErstellen rundenRight;
-		ButtonErstellen rundenValue1;
-		ButtonErstellen rundenValue10;
-		
-		ButtonErstellen zeit;
-		ButtonErstellen zeitLeft;
-		ButtonErstellen zeitRight;
-		ButtonErstellen zeitValue1;
-		ButtonErstellen zeitValue10;
-		
-		ButtonErstellen zuruck;
-		ButtonErstellen start;
-		
-				
-		private SpriteBatch batch;
+
+	ButtonErstellen zuruck;
+	ButtonErstellen start;
+
+	private SpriteBatch batch;
 
 	protected LocalGamePrepareState(GameStateManager gsm) {
 		super(gsm);
@@ -47,6 +67,21 @@ public class LocalGamePrepareState extends GameState {
 		
 		batch = new SpriteBatch();    	
     	buttonSet();
+    	
+    	stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		
+
+		// Namenseingabefeld
+		/**
+		 * @author pbg2h15ago, pbg2h15ani
+		 */
+		skinName = new Skin(Gdx.files.internal("uiskin.json"));
+		
+		txfName = new TextField("", skinName);
+		txfName.setPosition(330, 375);
+		txfName.setSize(200, 50);
+		stage.addActor(txfName);
 	}
 
 	@Override
@@ -57,30 +92,132 @@ public class LocalGamePrepareState extends GameState {
 
 	@Override
 	public void render() {
+		/**
+		 * @author pbg2h15ala, pbg2h15ago
+		 */
 		
-		 Gdx.gl.glClearColor(1, 1, 1, 1);
-         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-         batch.begin();
-         
-         name.render(batch);
-         changeStage.render(batch);
-         
-         runden.render(batch);
-     	 rundenLeft.render(batch);
-     	 rundenValue1.render(batch);
-     	 rundenValue10.render(batch);
-     	 rundenRight.render(batch);
-     	 
-     	 zeit.render(batch);
-    	 zeitLeft.render(batch);
-    	 zeitValue1.render(batch);
-    	 zeitValue10.render(batch);
-    	 zeitRight.render(batch);
-         
-         start.render(batch);
-         zuruck.render(batch);
+		//Zurückbutton
+		
+		if(zuruck.isClicked()){
+			gsm.setState(GameStateManager.MAIN);
+		}
+		
+		// Spiel starten
+		
+		if(start.isClicked()){
+			gsm.setState(GameStateManager.GAME);
+		}
+		
+		/**
+		 *  @author pbg2h15ala, pbg2h15ago
+		 */
+		
+		// Stage einstellen
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
 
-         batch.end();	
+		if (changeStageLeft.isClicked()) {
+			mapNr--;
+			if (mapNr < 1) {
+				mapNr = 3;
+			}
+			changeStageValue.setTexture("textures/stage_" + mapNr + ".png");
+			System.out.println(mapNr);
+		}
+		if (changeStageRight.isClicked()) {
+			mapNr++;
+			if (mapNr > 3) {
+				mapNr = 1;
+			}
+			changeStageValue.setTexture("textures/stage_" + mapNr + ".png");
+		}
+		
+		/**
+		 * @author pbg2h15ago
+		 */
+		
+		// Zeit einstellen
+		
+		if(zeitRight.isClicked() && rundenZeitMin <= 5){
+			rundenZeitSek += 10;
+			if(rundenZeitSek >= 10 && rundenZeitMin == 5){
+				rundenZeitSek = 0;
+				rundenZeitMin = 1;
+			}else{
+			if(rundenZeitSek >= 60){
+				
+					rundenZeitSek = 0;
+					rundenZeitMin += 1;	
+				}
+				
+			}
+		}
+		if(rundenZeitSek < 10){
+			zeitValue.draw(batch, ""+rundenZeitMin+":0"+rundenZeitSek, 380, 195);
+		}else{
+			zeitValue.draw(batch, ""+rundenZeitMin+":"+rundenZeitSek, 380, 195);
+		}
+		
+
+		if(zeitLeft.isClicked() && rundenZeitMin >= 1){
+			rundenZeitSek -= 10;
+			if(rundenZeitSek < 0){
+				if(rundenZeitMin <= 1 && rundenZeitSek < 0){
+					rundenZeitMin = 5;
+					rundenZeitSek = 0;
+				}else{
+					rundenZeitSek = 50;
+					rundenZeitMin -= 1;
+				}
+				
+			}
+			
+		}
+		/**
+		 * @author pbg2h15ago,pbg2h15aln
+		 */
+		
+		//Gewinnrundenanzahl
+      if(rundenRight.isClicked() && rundenAnzahl < 5){
+        	rundenAnzahl++;
+      }
+        
+        
+       if(rundenLeft.isClicked() && rundenAnzahl > 1){
+        	rundenAnzahl--;
+       }
+       
+       setNumberTo(rundenAnzahl, rundenValue10, rundenValue1);
+		
+		/**
+		 *  @author pbg2h15ala
+		 */
+
+		name.render(batch);
+
+
+		changeStage.render(batch);
+		changeStageLeft.render(batch);
+		changeStageRight.render(batch);
+		changeStageValue.render(batch);
+
+		runden.render(batch);
+		rundenLeft.render(batch);
+		rundenValue1.render(batch);
+		rundenValue10.render(batch);
+		rundenRight.render(batch);
+
+		zeit.render(batch);
+		zeitLeft.render(batch);
+
+		zeitRight.render(batch);
+
+		start.render(batch);
+		zuruck.render(batch);
+
+		batch.end();
+		stage.draw();
 	}
 
 	@Override
@@ -89,32 +226,77 @@ public class LocalGamePrepareState extends GameState {
 		
 	}
 	private void buttonSet(){
-	    
-		//1. Zeile
-    	
-    	name = new ButtonErstellen(110, 350, "textures/name.png");
-    	changeStage = new  ButtonErstellen(330, 350, "textures/stage.png");
-    	
-    	// 2. Zeile
-    	
-    	runden = new ButtonErstellen(110, 250, "textures/runden.png");
-    	rundenLeft = new ButtonErstellen(330, 250, "textures/arrow_left.png");
-    	rundenValue1 = new ButtonErstellen(380, 250, "textures/number.png");
-    	rundenValue10 = new ButtonErstellen(430, 250, "textures/number.png");
-    	rundenRight = new ButtonErstellen(480, 250, "textures/arrow_right.png");
-    	
-    	// 3. Zeile
-    	
-    	zeit = new ButtonErstellen(110, 150, "textures/zeit.png");
-    	zeitLeft = new ButtonErstellen(330, 150, "textures/arrow_left.png");
-    	zeitValue1 = new ButtonErstellen(380, 150, "textures/number.png");
-    	zeitValue10 = new ButtonErstellen(430, 150, "textures/number.png");
-    	zeitRight = new ButtonErstellen(480, 150, "textures/arrow_right.png");
-    	
-    	// 4. Zeile
-    	
-    	zuruck = new ButtonErstellen(110, 50, "textures/zuruck.png");
-    	start = new ButtonErstellen(330, 50, "textures/start.png");
+		
+		/**
+		 * @author pbg2h15ala
+		 */
+
+		// 0.Zeile
+
+		name = new ButtonErstellen(110, 375, "textures/name.png");
+
+
+		// 1. Zeile
+
+		changeStage = new ButtonErstellen(110, 285, "textures/stage.png");
+		changeStageLeft = new ButtonErstellen(330, 285, "textures/arrow_left.png");
+		changeStageValue = new texHelper(380, 285, "textures/stage_1.png");
+		changeStageRight = new ButtonErstellen(480, 285, "textures/arrow_right.png");
+
+		// 2. Zeile
+
+		runden = new ButtonErstellen(110, 195, "textures/runden.png");
+		rundenLeft = new ButtonErstellen(330, 195, "textures/arrow_left.png");
+		rundenValue1 = new texHelper(430, 195, "textures/number.png");
+		rundenValue10 = new texHelper(380, 195, "textures/number.png");
+		rundenRight = new ButtonErstellen(480, 195, "textures/arrow_right.png");
+
+		// 3. Zeile
+
+		zeit = new ButtonErstellen(110, 105, "textures/zeit.png");
+		zeitLeft = new ButtonErstellen(330, 105, "textures/arrow_left.png");
+		zeitRight = new ButtonErstellen(480, 105, "textures/arrow_right.png");
+
+		// 4. Zeile
+
+		zuruck = new ButtonErstellen(110, 15, "textures/zuruck.png");
+		start = new ButtonErstellen(330, 15, "textures/start.png");
 	}
+	
+	private class texHelper {
+		
+		/**
+		 *  @author pbg2h15ala
+		 *  entnommen aus Buttonerstellen
+		 */
+
+		private Texture texture;
+		private float x;
+		private float y;
+
+		private texHelper(float x, float y, String pfad) {
+			texture = new Texture(pfad);
+			this.x = x;
+			this.y = y;
+
+		}
+		public void render(SpriteBatch batch) {
+			batch.draw(texture, x, y);
+
+		}		
+		public void setTexture(String pfad){
+			texture = new Texture(pfad);
+		}
+	}
+	
+	private void setNumberTo(int number, texHelper value10, texHelper value1){
+		/**
+		 *  @author pbg2h15ala
+		 */
+		number%=100;
+		value10.setTexture("textures/number_" + number/10+".png");
+		value1.setTexture("textures/number_" + number%10+".png");
+	}
+	
 
 }
