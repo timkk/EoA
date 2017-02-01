b752e9ec6d8a0a8db3eff645e152bc8962085e68
pbg2h15awi <pbg2h15awi@pb.bib.de>
2017-02-01 10:09:09 +0100

    Kommentare GameStateManager und ButtonErstellen

--- EoA/src/de/bib/pbg2h15a/ButtonErstellen.java
+++ EoA/src/de/bib/pbg2h15a/ButtonErstellen.java
@@ -11,7 +11,7 @@ import com.badlogic.gdx.math.Rectangle;
  * @author pbg2h15ago 
  * @author pbg2h15awi 
  * @author pbg2h15aza
- *
+ * (Kommentiert von Kevin Wickel pbg2h15awi)
  */
 
 public class ButtonErstellen {
@@ -21,7 +21,13 @@ public class ButtonErstellen {
 	private float y;
 	
 	private boolean imClick;
-
+	
+	/**
+	 * Initialisiert den Button
+	 * @param x Position der x-Achse
+	 * @param y Position der y-Achse
+	 * @param pfad Dateipfad des Bildes
+	 */
 	public ButtonErstellen(float x, float y,String pfad) {
 		texture = new Texture(pfad);
 		this.x = x;
@@ -29,6 +35,10 @@ public class ButtonErstellen {
 		imClick=true;
 	}
 
+	/**
+	 * Prüft ob der Button geklickt wurde
+	 * @return true wenn auf den Button gedrückt wird, sonst false
+	 */
 	public boolean isClicked() {
 		if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
 			if(!imClick){
@@ -50,6 +60,10 @@ public class ButtonErstellen {
 		return false;
 	}
 
+	/**
+	 * Rendert den Button
+	 * @param batch Batch auf der gezeichnet wird
+	 */
 	public void render(SpriteBatch batch) {
 
 		batch.draw(texture, x, y);
